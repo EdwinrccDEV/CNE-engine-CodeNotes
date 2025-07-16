59e2ffdbfe52ef5982e1e03a75d8e4de5b5ae21c
Edwin_rcc [DEV] <edwincastchi14@gmail.com>
2025-07-15 22:31:30 -0700

    Update ALL notes.hx

--- ALL notes.hx
+++ ALL notes.hx
@@ -1,3 +1,55 @@
 /*
 CodeName Engine Code notes
 */
+
+// Hud propieties
+scoreTxt
+missesTxt
+acurracy
+healthBar
+healthBarBg
+	// Aplicabe propieties
+	Paths.font('FontName.ttf');
+	size = 0;
+	x = 0;
+	y = 0;
+	angle  = 0;
+	visible = null; // true, false
+	alignment = "center"; // left, center, right
+
+
+
+// Change noteSkins
+function onNoteCreation(note){
+    note.noteSprite = "game/notes/NoteSkinName";
+}
+function onStrumCreation(note){
+    note.sprite = "game/notes/NoteSkinName";
+}
+
+
+
+// Note hit Events
+// Dad
+function onDadHit(){
+}
+// Player
+function onPlayerHit(){
+}
+// Both
+function onNoteHit(){
+}
+	// Events on each note
+	function onIDKHit(e){
+    switch(e.direction){
+        case 0: // Left
+	
+        case 1: // Down
+	
+        case 2: // Up
+	
+        case 3: // Right
+    }
+}
+
+
